3899e694da25e3e97df94d6ab7a24f1951f2e9e9
suryamurugan <surya3542live@gmail.com>
2026-05-26 01:51:57 +0530

    fix: update logger configuration in server setup

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,9 @@ const port = isNaN(env_port) ? 3001 : env_port;
 
 async function main() {
   const app = Fastify({
-    logger: { file: '/home/logs/app.log', level: 'debug' },
+    logger: { 
+//      file: './app.log', 
+      level: 'debug' },
     trustProxy: true,
   });
   // Zod Validation Compiler Setup
